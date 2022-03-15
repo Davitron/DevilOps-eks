433d1470c2b65d453be60a5c6ddcd923e0de9480
devops-cloudbit <devops@cloudbit.dev>
2022-03-15 18:55:10 +0100

    minor minor 2

--- test/vpc.tf
+++ test/vpc.tf
@@ -4,5 +4,7 @@ module "vpc" {
   cidr           = "10.0.0.0/16"
   private_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets_cidr  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  providers = aws
+  providers = {
+    aws = aws.eu-west-2
+   }
 }
