64dedf9c2096aa428006947e3a4c62ed9960534b
devops-cloudbit <devops@cloudbit.dev>
2022-03-15 18:05:23 +0100

    add variables

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,3 +1,16 @@
 variable "default_vpc_id" {
   description = "the id of default vpc"
 }
+
+variable "cidr" {
+  type = string
+}
+
+variable "private_subnets_cidr" {
+  type = list(string)
+}
+
+
+variable "public_subnets_cidr" {
+  type = list(string)
+}
