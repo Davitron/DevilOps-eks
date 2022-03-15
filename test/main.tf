c54580f9463435fbd2208742f04faa97bc36f5ad
devops-cloudbit <devops@cloudbit.dev>
2022-03-15 18:56:56 +0100

    minor fix 3

--- test/main.tf
+++ test/main.tf
@@ -21,5 +21,5 @@ provider "aws" {
 
 
 data "aws_vpc" "default" {
-  provider = aws.eu-west-2
+  provider = aws
 }
--- test/vpc.tf
+++ test/vpc.tf
@@ -5,6 +5,6 @@ module "vpc" {
   private_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets_cidr  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   providers = {
-    aws = aws.eu-west-2
+    aws = aws
    }
 }
