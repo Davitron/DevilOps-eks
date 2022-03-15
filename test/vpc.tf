d2484c1c8576916c2be3223cba911cd0f76489c6
devops-cloudbit <devops@cloudbit.dev>
2022-03-15 18:02:50 +0100

    complete variables

--- test/vpc.tf
+++ test/vpc.tf
@@ -1,4 +1,7 @@
 module "vpc" {
   source         = "../modules/vpc"
   default_vpc_id = data.aws_vpc.default.id
+  cidr           = "10.0.0.0/16"
+  private_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets_cidr  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
